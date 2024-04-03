63bd08ead151d5654ecba014dde206ddda8acc52
KarimGhazzi <kelghazzi.ieu2021@student.ie.edu>
2024-04-03 21:51:09 +0200

    again x3

--- chatbot/tests/unit_tests.py
+++ chatbot/tests/unit_tests.py
@@ -1,7 +1,12 @@
 # tests/test_my_code.py
 from chatbot.app2 import add_numbers
+import unittest
+
 
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
     assert add_numbers(-2, 3) == 1
     assert add_numbers(-2, -3) == -5
+
+if __name__ == '__main__':
+    unittest.main()
