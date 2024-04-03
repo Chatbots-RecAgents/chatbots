be8a7b4cada2fca638b5fd14c2c50fabef155e32
KarimGhazzi <kelghazzi.ieu2021@student.ie.edu>
2024-04-03 22:00:49 +0200

    test x5

--- chatbot/unit_tests.py
+++ chatbot/unit_tests.py
@@ -1,10 +1,12 @@
-# tests/test_my_code.py
-from chatbot.app2 import add_numbers
+# test_math_operations.py
+
 import unittest
+from chatbot.app2 import add_numbers
+
+class TestAddNumbers(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add_numbers(2, 3), 5)
 
 
-def test_add_numbers():
-    assert add_numbers(2, 3) == 5
-    assert add_numbers(-2, 3) == 1
-    assert add_numbers(-2, -3) == -5
-
+if __name__ == '__main__':
+    unittest.main()
